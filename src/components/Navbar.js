f670b26a6a929a7aed4852f1f6d38a43e6457ad3
Henry1998N <henry.nur137@gmail.com>
2023-04-15 11:55:13 +0300

    changed nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,31 @@
-import React from 'react'
-import  './css/Navbar.css'
-import { Link } from 'react-router-dom';
-
+import React from "react";
+import "./css/Navbar.css";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav>
-    <div class="nav-container">
-    <Link to={'/Home'}> <a class="logo">LOGO</a></Link>
-      <ul class="nav-menu">
-    <Link to={'/Home'}>  <li><a>Home</a></li></Link>
-    <Link to={'/Apartments'}><li><a>Apartments</a></li></Link>
-      </ul>
-    </div>
-  </nav>
-  )
-}
+      <div class="nav-container">
+        <Link to={"/Home"}>
+          {" "}
+          <a class="logo">LOGO</a>
+        </Link>
+        <ul class="nav-menu">
+          <Link to={"/Home"}>
+            {" "}
+            <li>
+              <a>Home</a>
+            </li>
+          </Link>
+          <Link to={"/Apartments"}>
+            <li>
+              <a>Apartments</a>
+            </li>
+          </Link>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
