487a513f15bfa04992ad271860cf5363e93df0eb
Braa Knaane <braa97@windowslive.com>
2023-04-16 11:31:28 +0300

    made the Landing page responsive, and added random color generator temporarrly

--- src/components/Apartments/Apartments.js
+++ src/components/Apartments/Apartments.js
@@ -1,24 +1,31 @@
 import React, { useEffect, useState } from "react";
 import "./Apartments.css";
-import Apartment from '../Apartment/Apartment'
+import Apartment from "../Apartment/Apartment";
 import ApiManager from "../../apiManager/apiManager";
 
 const Apartments = () => {
   const [apartments, setApartments] = useState([]);
 
   useEffect(() => {
-    let apiManger = new ApiManager()
+    let apiManger = new ApiManager();
     let fetchApartments = async () => {
-      let apartments = await apiManger.getApartments()
-      setApartments(apartments)
+      let apartments = await apiManger.getApartments();
+      setApartments(apartments);
     };
     fetchApartments();
   }, []);
 
   return (
-  <div className="apartments-container">
-    {apartments.map(apartment => <Apartment key={apartment._id} apartment={apartment} />)}
-  </div>
+    <>
+    <div className="apartments-header-label">
+      <label>Apartments</label>
+    </div>
+      <div className="apartments-container">
+        {apartments.map((apartment) => (
+          <Apartment key={apartment._id} apartment={apartment} />
+        ))}
+      </div>
+    </>
   );
 };
 
