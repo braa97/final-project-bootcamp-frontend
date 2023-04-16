2f0bbee680c4a29f43f26e00561052255daed6ce
TamerDarwish <darwishtamer99@gmail.com>
2023-04-16 15:45:13 +0300

    a

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Residents from './components/Residents/Residents'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 
+
+
 const App = () => {
   return (
     <Router> 
