eca8905080ccddba4c7fd77ab46869cdcdf921e7
TamerDarwish <darwishtamer99@gmail.com>
2023-04-17 01:23:23 +0300

    add ResidentDetails Component

---
+++ src/components/ResidentDetails/ResidentDetails.js
@@ -0,0 +1,17 @@
+import './ResidentDetails.css'
+
+const ResidentDetails = ({residentData}) => {
+  return (
+    <div className='details-container'>
+        <div> Name: {residentData.firstName + residentData.lastName }</div>
+        <div> BirthDate: {residentData.birthDate}</div>
+        <div> Gender: {residentData.gender}</div>
+        <div> Personal Budget: {residentData.residentBudget}</div>
+        <div>Allergies: {residentData.allergies.map(allergy => <span>{allergy+' '}</span>)}</div>
+        
+    </div>
+   
+  )
+}
+
+export default ResidentDetails
