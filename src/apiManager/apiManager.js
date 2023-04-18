b31c5b11ab3cf90aff5a9abd6f0e744b5008d7c2
Henry1998N <henry.nur137@gmail.com>
2023-04-18 09:16:05 +0300

    conflict

--- src/apiManager/apiManager.js
+++ src/apiManager/apiManager.js
@@ -4,35 +4,45 @@ const ApiManager = function () {
   //Create try and catch
 
   //Create get Data function that calls axios and implement it in each function
-  const ajaxCall = async(url) => {
+  const ajaxCall = async (url) => {
     try {
-      let response = await axios.get(url)
-      return response.data
-    }
-    catch(error) {
+      let response = await axios.get(url);
+      return response.data;
+    } catch (error) {
       console.log(error);
     }
-  }
-
-  const getResidentsByApartmentName = async(apartmentName) => {
-    const response = await ajaxCall(process.env.REACT_APP_RESIDENTS_ROUTE + `/${apartmentName}`);
-    return response
   };
 
-  const getApartments = async() => {
+  const getResidentsByApartmentName = async (apartmentName) => {
+    const response = await ajaxCall(
+      process.env.REACT_APP_RESIDENTS_ROUTE + `/${apartmentName}`
+    );
+    return response;
+  };
+
+  const getApartments = async () => {
     const response = await ajaxCall(process.env.REACT_APP_APARTMENTS_ROUTE);
-    return response
+    return response;
   };
 
-  const getApartmentByName = async(apartmentName) => {
-    const response = await ajaxCall(`${process.env.REACT_APP_SINGLE_APARTMENT_ROUTE}` + `/${apartmentName}`)
-    return response
-  }
+  const getApartmentByName = async (apartmentName) => {
+    const response = await ajaxCall(
+      `${process.env.REACT_APP_SINGLE_APARTMENT_ROUTE}` + `/${apartmentName}`
+    );
+    return response;
+  };
+  const getResidentDetails = async (residentId) => {
+    const response = await ajaxCall(
+      `${process.env.REACT_APP_RESIDENT_DETAILS_ROUTE}` + `/${residentId}`
+    );
+    return response;
+  };
 
   return {
     getResidentsByApartmentName: getResidentsByApartmentName,
     getApartments: getApartments,
     getApartmentByName: getApartmentByName,
+    getResidentDetails: getResidentDetails,
   };
 };
 
