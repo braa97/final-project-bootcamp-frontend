cf9a324319027341381ee4092ff275a2826526f2
Braa Knaane <braa97@windowslive.com>
2023-04-18 17:13:41 +0300

    Added a loading wheel and a server error page

--- src/components/ApartmentsPage/Apartments.js
+++ src/components/ApartmentsPage/Apartments.js
@@ -1,23 +1,26 @@
-import React, { useEffect, useState } from "react";
 import "./Apartments.css";
+import React, { useEffect, useState } from "react";
 import Apartment from "../Apartment/Apartment";
 import ApiManager from "../../apiManager/apiManager";
+import LoadingWheel from "../LoadingWheel/LoadingWheel";
+
 import { useNavigate } from "react-router";
 
 const Apartments = () => {
   const [apartments, setApartments] = useState([]);
-  const navigate = useNavigate()
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-    const apiManager = new ApiManager()
-    const checkServerConnection = async() => {
-      let response = await apiManager.checkServerConnection()
+  useEffect(() => {
+    const apiManager = new ApiManager();
+    const checkServerConnection = async () => {
+      let response = await apiManager.checkServerConnection();
       if (response.status != 200) {
-        navigate('/server-error')
+        navigate("/server-error");
       }
-    }
-    checkServerConnection()
-  }, [])
+    };
+    checkServerConnection();
+  }, []);
 
   useEffect(() => {
     const apiManager = new ApiManager();
@@ -25,17 +28,17 @@ const Apartments = () => {
     let fetchApartments = async () => {
       let apartments = await apiManager.getApartments();
       setApartments(apartments);
+      setLoading(false);
     };
     fetchApartments();
   }, []);
 
-
-
   return (
     <>
-    <div className="apartments-header-label">
-      <label>{apartments.length != 0 ? "Apartments" : null}</label>
-    </div>
+      <div className="apartments-header-label">
+        <label>{apartments.length != 0 ? "Apartments" : null}</label>
+      </div>
+      <div className="loading-wheel">{loading ? <LoadingWheel /> : null}</div>
       <div className="apartments-container">
         {apartments.map((apartment) => (
           <Apartment key={apartment._id} apartment={apartment} />
