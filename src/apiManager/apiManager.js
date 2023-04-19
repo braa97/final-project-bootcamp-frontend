e2251040879c5e47e4a2f02c79784ad2103e8ca0
Abdallah Abu khdair <71616389+abdullahab1968@users.noreply.github.com>
2023-04-19 11:07:07 +0300

    add medicatioon appointment edit

--- src/apiManager/apiManager.js
+++ src/apiManager/apiManager.js
@@ -52,7 +52,7 @@ const ApiManager = function () {
 
   const addMedicalAppointment = async(residentId, newMedicalAppointment) => {
     console.log(residentId)
-    const res = await axios.put(process.env.REACT_APP_RESIDENTS_ROUTE  + `/medicalAppointment/${residentId}`, newMedicalAppointment);
+    const res = await axios.put(process.env.REACT_APP_SERVER_ROUTE  + `/resident/medicalAppointment/${residentId}`, {newAppointment: newMedicalAppointment});
     return res
   }
 
