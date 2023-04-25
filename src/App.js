ed362ed6102df8a0358c70d52808eafd6164ec53
Henry1998N <henry.nur137@gmail.com>
2023-04-25 15:24:34 +0300

    resolved  conflict

--- src/App.js
+++ src/App.js
@@ -8,20 +8,23 @@ import Residents from "./components/ResidentsPage/Residents";
 import ResidentInfoPage from "./components/ResidentInfoPage/ResidentInfoPage/ResidentInfoPage";
 import Home from "./components/Home/Home";
 import ServerError from "./components/ServerError/ServerError";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
-import { useLocation } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useNavigate,
+  useLocation,
+} from "react-router-dom";
 import ApiManager from "./apiManager/apiManager";
 import GoogleButton from "./components/GoogleLogin/GoogleButton";
 import SignIn from "./components/SignIn/SignIn";
 
 const App = () => {
-  const navigate = useNavigate();
-  const location = useLocation();
+  const navigate = useNavigate;
   const [isCollapsed, setCollapsed] = useState(false);
   const [darkMode, setDarkMode] = useState(false);
   const [isLoggedin, setIsLoggedin] = useState(
-    localStorage.getItem("instructorId")
+    localStorage.getItem("instructorId") || null
   );
 
   const handleSidebarCollapse = function () {
@@ -39,38 +42,43 @@ const App = () => {
   }, []);
 
   return (
-    <div className="main-page-container">
-      {isLoggedin ? (
-        <>
-          <div className="navbar-container">
-            <Navbar
-              handleSidebarCollapse={handleSidebarCollapse}
-              handleDarkMode={handleDarkMode}
+    <Router>
+      <div className="main-page-container">
+        {isLoggedin ? (
+          <>
+            <div className="navbar-container">
+              <Navbar
+                handleSidebarCollapse={handleSidebarCollapse}
+                handleDarkMode={handleDarkMode}
+              />
+            </div>
+            <div className="sidebar-container">
+              <Sidebar isCollapsed={isCollapsed} />
+            </div>
+          </>
+        ) : null}
+        <div className="main-content">
+          <Routes>
+            {/* <Route path="/" element={<Dashboard/>} /> */}
+            <Route path="/" element={<Apartments />} />
+            <Route path="/residents/:apartmentName" element={<Residents />} />
+            <Route
+              path="/resident/:residentId"
+              element={<ResidentInfoPage />}
             />
-          </div>
-          <div className="sidebar-container">
-            <Sidebar isCollapsed={isCollapsed} />
-          </div>
-        </>
-      ) : null}
-      <div className="main-content">
-      <Routes>
-        {/* <Route path="/" element={<Dashboard/>} /> */}
-        <Route path="/" element={<Apartments />} />
-        <Route path="/residents/:apartmentName" element={<Residents />} />
-        <Route path="/resident/:residentId" element={<ResidentInfoPage />} />
-        <Route path="/server-error" element={<ServerError />} />
-        {/* <Route path="*" element={<NotFound />} /> */}
-        <Route
-          path="/login"
-          element={
-            <SignIn setIsLoggedin={setIsLoggedin} isLoggedin={isLoggedin} />
-          }
-        />
-        {/* <Route path="/:instructorId/dashboard" element={<Home />} /> */}
-      </Routes>
+            <Route path="/server-error" element={<ServerError />} />
+            {/* <Route path="*" element={<NotFound />} /> */}
+            <Route
+              path="/login"
+              element={
+                <SignIn setIsLoggedin={setIsLoggedin} isLoggedin={isLoggedin} />
+              }
+            />
+            {/* <Route path="/:instructorId/dashboard" element={<Home />} /> */}
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 };
 
