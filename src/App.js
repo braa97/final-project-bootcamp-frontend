92fffe3813591f626790b4dc90dcf1e6724905d1
Braa Knaane <braa97@windowslive.com>
2023-04-27 02:40:46 +0300

    Fixed the navbar and sidebar and added a main page sort of thing to the app component so that all our routes will be nested there

--- src/App.js
+++ src/App.js
@@ -1,94 +1,86 @@
-import './App.css'
-import './global-styles/dark-style.css'
-import React, { useEffect, useState } from 'react'
-// import Navbar from "./components/Nabar-Sidebar/Navbar/Navbar";
-// import Sidebar from "./components/Nabar-Sidebar/Sidebar/Sidebar";
-import Apartments from './components/ApartmentsPage/Apartments'
-import Residents from './components/ApartmentInfo/ResidentsPage/ResidentsPage'
-import ResidentInfoPage from './components/ResidentInfoPage/ResidentInfoPage/ResidentInfoPage'
-import Home from './components/Home/Home'
-import ServerError from './components/ServerError/ServerError'
-import {
-    // BrowserRouter as Router,
-    Route,
-    Routes,
-    useNavigate,
-    // useLocation,
-} from 'react-router-dom'
-// import ApiManager from "./apiManager/apiManager";
-// import GoogleButton from "./components/GoogleLogin/GoogleButton";
-import SignIn from './components/SignIn/SignIn'
-import CoordinatorHome from './components/Coordinator_Dashboard/Coordinator_Home/CoordinatorHome'
-import NewResident from './components/ApartmentInfo/NewResident/NewResident'
+import "./App.css";
+import "./global-styles/dark-style.css";
+import React, { useEffect, useState } from "react";
+import Apartments from "./components/ApartmentsPage/Apartments";
+import Residents from "./components/ApartmentInfo/ResidentsPage/ResidentsPage";
+import ResidentInfoPage from "./components/ResidentInfoPage/ResidentInfoPage/ResidentInfoPage";
+import Home from "./components/Home/Home";
+import ServerError from "./components/ServerError/ServerError";
+import Navbar from "./components/Navbar/Navbar";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import SignIn from "./components/SignIn/SignIn";
+import CoordinatorHome from "./components/Coordinator_Dashboard/Coordinator_Home/CoordinatorHome";
+import NewResident from "./components/ApartmentInfo/NewResident/NewResident";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 const App = () => {
-    const navigate = useNavigate()
-    const [isCollapsed, setCollapsed] = useState(false)
-    const [darkMode, setDarkMode] = useState(false)
-    const [isLoggedin, setIsLoggedin] = useState(
-        localStorage.getItem('instructorId') || null
-    )
+  const navigate = useNavigate();
+  const [isLoggedin, setIsLoggedin] = useState(
+    localStorage.getItem("instructorId") || null
+  );
 
-    const handleSidebarCollapse = function () {
-        setCollapsed(!isCollapsed)
+  useEffect(() => {
+    if (!isLoggedin) {
+      navigate("/login");
     }
+  }, [isLoggedin]);
 
-    const handleDarkMode = () => {
-        setDarkMode(!darkMode)
-    }
-
-    useEffect(() => {
-        if (!isLoggedin) {
-            navigate('/login')
-        }
-    }, [isLoggedin, navigate])
-
-    return (
-        <div>
-            <div>
-                <Home
-                    handleSidebarCollapse={handleSidebarCollapse}
-                    handleDarkMode={handleDarkMode}
-                    isCollapsed={isCollapsed}
-                    isLoggedin={isLoggedin}
-                />
-                <Routes>
-                    <Route path='/apartments' element={<Apartments />} />
-                    <Route
-                        path='/apartments/apartment-info/:apartmentName'
-                        element={<Residents />}
-                    />
-                    <Route
-                        path='/apartments/apartment-info/resident/:residentId'
-                        element={<ResidentInfoPage />}
-                    />
-                    <Route
-                        path='/apartments/apartment-info/resident/new-resident'
-                        element={<NewResident />}
-                    />
-                    <Route path='/server-error' element={<ServerError />} />
-                    {/* <Route path="*" element={<NotFound />} /> */}
-                    <Route
-                        path='/login'
-                        element={
-                            <SignIn
-                                setIsLoggedin={setIsLoggedin}
-                                isLoggedin={isLoggedin}
-                            />
-                        }
-                    />
-                    <Route
-                        path='/apartments/:instructorId'
-                        element={<Apartments />}
-                    />
-                    <Route
-                        path='/Coordinator/dashboard/:id'
-                        element={<CoordinatorHome />}
-                    />
-                </Routes>
+  return (
+    <div className="main-page-container">
+      <>
+        {isLoggedin ? (
+          <>
+            <div className="navbar-container">
+              <Navbar />
             </div>
-        </div>
-    )
-}
+            <div className="sidebar-container">
+              <Sidebar />
+            </div>
+          </>
+        ) : null}
+        <Routes>
+          <Route
+            path="/login"
+            element={
+              <SignIn setIsLoggedin={setIsLoggedin} isLoggedin={isLoggedin} />
+            }
+          />
+        </Routes>
+      </>
 
-export default App
+      <div className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/apartments" element={<Apartments />} />
+          <Route
+            path="/apartments/apartment-info/:apartmentName"
+            element={<Residents />}
+          />
+          <Route
+            path="/apartments/apartment-info/resident/:residentId"
+            element={<ResidentInfoPage />}
+          />
+          <Route
+            path="/apartments/apartment-info/resident/new-resident"
+            element={<NewResident />}
+          />
+          <Route path="/server-error" element={<ServerError />} />
+          {/* <Route path="*" element={<NotFound />} /> */}
+          {/* <Route
+            path="/login"
+            element={
+              <SignIn setIsLoggedin={setIsLoggedin} isLoggedin={isLoggedin} />
+            }
+          /> */}
+          <Route path="/apartments/:instructorId" element={<Apartments />} />
+          <Route
+            path="/Coordinator/dashboard/:id"
+            element={<CoordinatorHome />}
+          />
+        </Routes>
+      </div>
+    </div>
+  );
+};
+
+export default App;
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,189 +1,83 @@
-import './Sidebar.css'
-import DashboardIcon from '@mui/icons-material/Dashboard'
-import PersonOutlineIcon from '@mui/icons-material/PersonOutline'
-// import LocalShippingIcon from '@mui/icons-material/LocalShipping'
-// import CreditCardIcon from '@mui/icons-material/CreditCard'
-import StoreIcon from '@mui/icons-material/Store'
-import InsertChartIcon from '@mui/icons-material/InsertChart'
-import SettingsApplicationsIcon from '@mui/icons-material/SettingsApplications'
-import ExitToAppIcon from '@mui/icons-material/ExitToApp'
-import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone'
-import SettingsSystemDaydreamOutlinedIcon from '@mui/icons-material/SettingsSystemDaydreamOutlined'
-import PsychologyOutlinedIcon from '@mui/icons-material/PsychologyOutlined'
-import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined'
-import { Link } from 'react-router-dom'
+import "./Sidebar.css";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import StoreIcon from "@mui/icons-material/Store";
+import InsertChartIcon from "@mui/icons-material/InsertChart";
+import SettingsApplicationsIcon from "@mui/icons-material/SettingsApplications";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import SettingsSystemDaydreamOutlinedIcon from "@mui/icons-material/SettingsSystemDaydreamOutlined";
+import PsychologyOutlinedIcon from "@mui/icons-material/PsychologyOutlined";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
-    return (
-        <div className='sidebar'>
-            <div className='top'>
-                <Link to='/' style={{ textDecoration: 'none' }}>
-                    <span className='logo'>
-                        Care
-                        <br />
-                        Companion+
-                    </span>
-                </Link>
-            </div>
-            <hr />
-            <div className='center'>
-                <ul>
-                    <p className='title'>MAIN</p>
-                    <li>
-                        <DashboardIcon className='icon' />
-                        <span>Dashboard</span>
-                    </li>
-                    <p className='title'>LISTS</p>
-                    <Link to='/users' style={{ textDecoration: 'none' }}>
-                        <li>
-                            <PersonOutlineIcon className='icon' />
-                            <span>Residents</span>
-                        </li>
-                    </Link>
-                    <Link to='/products' style={{ textDecoration: 'none' }}>
-                        <li>
-                            <StoreIcon className='icon' />
-                            <span>Apartments</span>
-                        </li>
-                    </Link>
+  const logout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("instructorId");
+    window.location.reload();
+  };
 
-                    <p className='title'>USEFUL</p>
-                    <li>
-                        <InsertChartIcon className='icon' />
-                        <span>Stats</span>
-                    </li>
-                    <li>
-                        <NotificationsNoneIcon className='icon' />
-                        <span>Notifications</span>
-                    </li>
-                    <p className='title'>SERVICE</p>
-                    <li>
-                        <SettingsSystemDaydreamOutlinedIcon className='icon' />
-                        <span>Medications</span>
-                    </li>
-                    <li>
-                        <PsychologyOutlinedIcon className='icon' />
-                        <span>Report</span>
-                    </li>
-                    <li>
-                        <SettingsApplicationsIcon className='icon' />
-                        <span>Settings</span>
-                    </li>
-                    <p className='title'>USER</p>
-                    <li>
-                        <AccountCircleOutlinedIcon className='icon' />
-                        <span>Profile</span>
-                    </li>
-                    <li>
-                        <ExitToAppIcon className='icon' />
-                        <span>Logout</span>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="sidebar">
+      <div className="center">
+        <ul>
+          <hr className="sidebar-divider" />
+          <Link to="/" style={{ textDecoration: "none" }}>
+            <li>
+              <DashboardIcon className="icon" />
+              <span>Dashboard</span>
+            </li>
+          </Link>
+          <hr className="sidebar-divider" />
+          <Link to="/users" style={{ textDecoration: "none" }}>
+            <li>
+              <PersonOutlineIcon className="icon" />
+              <span>Residents</span>
+            </li>
+          </Link>
+          <Link to="/apartments" style={{ textDecoration: "none" }}>
+            <li>
+              <StoreIcon className="icon" />
+              <span>Apartments</span>
+            </li>
+          </Link>
 
-export default Sidebar
+          <hr className="sidebar-divider" />
+          <li>
+            <InsertChartIcon className="icon" />
+            <span>Stats</span>
+          </li>
+          <li>
+            <NotificationsNoneIcon className="icon" />
+            <span>Notifications</span>
+          </li>
+          <hr className="sidebar-divider" />
+          <li>
+            <SettingsSystemDaydreamOutlinedIcon className="icon" />
+            <span>Medications</span>
+          </li>
+          <li>
+            <PsychologyOutlinedIcon className="icon" />
+            <span>Report</span>
+          </li>
+          <li>
+            <SettingsApplicationsIcon className="icon" />
+            <span>Settings</span>
+          </li>
+          <hr className="sidebar-divider" />
+          <li>
+            <AccountCircleOutlinedIcon className="icon" />
+            <span>Profile</span>
+          </li>
+          <li onClick={logout}>
+            <ExitToAppIcon className="icon" />
+            <span>Logout</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-
-// old sidebar with links:
-
-// import './Sidebar.css'
-// import DashboardIcon from '@mui/icons-material/Dashboard'
-// import PersonOutlineIcon from '@mui/icons-material/PersonOutline'
-// import CalendarMonthIcon from '@mui/icons-material/CalendarMonth'
-// import BloodtypeIcon from '@mui/icons-material/Bloodtype'
-// import ApartmentIcon from '@mui/icons-material/Apartment'
-// import InsertChartIcon from '@mui/icons-material/InsertChart'
-// import ExitToAppIcon from '@mui/icons-material/ExitToApp'
-// import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone'
-// import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined'
-// import SummarizeIcon from '@mui/icons-material/Summarize'
-// import { useNavigate } from 'react-router-dom'
-// import { Link } from 'react-router-dom'
-
-// const Sidebar = ({ isCollapsed }) => {
-//     const navigate = useNavigate()
-
-//     const Logout = () => {
-//         localStorage.removeItem("instructorId")
-//         localStorage.removeItem("token")
-//         window.location.reload()
-//     }
-//     return (
-//         <div className={`sidebar ${isCollapsed && 'collapse'}`}>
-//             <div className='center'>
-//                 <ul>
-//                     <p className='sidebar-title'>MAIN</p>
-//                     <Link to='/'>
-//                         <li>
-//                             <DashboardIcon className='side-icon' />
-//                             <span className='sidebar-link'>Dashboard</span>
-//                         </li>
-//                     </Link>
-//                     <p className='sidebar-title'>LISTS</p>
-//                     <Link to='/residents'>
-//                         <li>
-//                             <PersonOutlineIcon className='side-icon' />
-//                             <span className='sidebar-link'>Residents</span>
-//                         </li>
-//                     </Link>
-//                     <Link to='/apartments'>
-//                         <li>
-//                             <ApartmentIcon className='side-icon' />
-//                             <span className='sidebar-link'>Apartments</span>
-//                         </li>
-//                     </Link>
-//                     <Link to='/appointments'>
-//                         <li>
-//                             <CalendarMonthIcon className='side-icon' />
-//                             <span className='sidebar-link'>Meetings</span>
-//                         </li>
-//                     </Link>
-//                     <p className='sidebar-title'>SERVICE</p>
-//                     <Link to='/medications'>
-//                         <li>
-//                             <BloodtypeIcon className='side-icon' />
-//                             <span className='sidebar-link'>Medications</span>
-//                         </li>
-//                     </Link>
-//                     <p className='sidebar-title'>USEFUL</p>
-//                     <Link to='/report'>
-//                         <li>
-//                             <SummarizeIcon className='side-icon' />
-//                             <span className='sidebar-link'>Shift Report</span>
-//                         </li>
-//                     </Link>
-//                     <Link to='/notifications'>
-//                         <li>
-//                             <NotificationsNoneIcon className='side-icon' />
-//                             <span className='sidebar-link'>Notifications</span>
-//                         </li>
-//                     </Link>
-//                     <Link to='/statistics'>
-//                         <li>
-//                             <InsertChartIcon className='side-icon' />
-//                             <span className='sidebar-link'>Statistics</span>
-//                         </li>
-//                     </Link>
-//                     <p className='sidebar-title'>USER</p>
-//                     <Link to='/profile'>
-//                         <li>
-//                             <AccountCircleOutlinedIcon className='side-icon' />
-//                             <span className='sidebar-link'>Profile</span>
-//                         </li>
-//                     </Link>
-//                     <Link onClick={Logout}>
-//                         <li>
-//                             <ExitToAppIcon className='side-icon' />
-//                             <span className='sidebar-link'>Logout</span>
-//                         </li>
-//                     </Link>
-//                 </ul>
-//             </div>
-//         </div>
-//     )
-// }
-
-// export default Sidebar
+export default Sidebar;
