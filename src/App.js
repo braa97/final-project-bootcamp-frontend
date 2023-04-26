6fd32d6bcc38c7c7363e96b6be647cde343005fa
BadeaFreh <71319309+BadeaFreh@users.noreply.github.com>
2023-04-26 16:05:10 +0300

    added assets folder, styled signin page

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ const App = () => {
   const [darkMode, setDarkMode] = useState(false);
   const [isLoggedin, setIsLoggedin] = useState(
     localStorage.getItem("instructorId") || null
+    // true
   );
 
   const handleSidebarCollapse = function () {
