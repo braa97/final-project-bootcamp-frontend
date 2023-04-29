23628d53f4857c112d67e60da0d9fd87b9fd973c
BadeaFreh <71319309+BadeaFreh@users.noreply.github.com>
2023-04-29 19:10:40 +0300

    links for navbar features

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,53 +1,58 @@
-import "./Navbar.css";
-import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
-import LanguageOutlinedIcon from "@mui/icons-material/LanguageOutlined";
-import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import './Navbar.css'
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined'
+import LanguageOutlinedIcon from '@mui/icons-material/LanguageOutlined'
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
 // import FullscreenExitOutlinedIcon from "@mui/icons-material/FullscreenExitOutlined";
-import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
-import { Link } from "react-router-dom";
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined'
+import { Link } from 'react-router-dom'
 // import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
 // import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
 // import {logo} from '../../Assets/index'
 
 const Navbar = () => {
-  return (
-    <div className="navbar">
-      <div className="wrapper">
-        <Link to="/" style={{ textDecoration: "none" }}>
-          <span className="logo">
-            CareCompanion+
-          </span>
-        </Link>
-        <div className="search">
-          <input type="text" placeholder="Search..." />
-          <SearchOutlinedIcon />
+    return (
+        <div className='navbar'>
+            <div className='wrapper'>
+                <Link to='/' style={{ textDecoration: 'none' }}>
+                    <span className='logo'>CareCompanion+</span>
+                </Link>
+                <div className='search'>
+                    <input type='text' placeholder='Search...' />
+                    <SearchOutlinedIcon />
+                </div>
+                <div className='items'>
+                    <div className='item'>
+                        <Link to='/' style={{ textDecoration: 'none' }}>
+                            <LanguageOutlinedIcon className='icon' />
+                        </Link>
+                    </div>
+                    <div className='item'>
+                        <Link to='/' style={{ textDecoration: 'none' }}>
+                            <DarkModeOutlinedIcon className='icon' />
+                        </Link>
+                    </div>
+                    <div className='item'>
+                        <Link to='/' style={{ textDecoration: 'none' }}>
+                            <NotificationsNoneOutlinedIcon className='icon' />
+                        </Link>
+                        <div className='counter'>1</div>
+                    </div>
+                    <div className='item'>
+                        <Link to='/' style={{ textDecoration: 'none' }}>
+                            <img
+                                src='https://pngimg.com/uploads/man/small/man_PNG6509.png'
+                                alt=''
+                                className='avatar'
+                            />
+                        </Link>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className="items">
-          <div className="item">
-            <LanguageOutlinedIcon className="icon" />
-            RTL
-          </div>
-          <div className="item">
-            <DarkModeOutlinedIcon className="icon" />
-          </div>
-          <div className="item">
-            <NotificationsNoneOutlinedIcon className="icon" />
-            <div className="counter">1</div>
-          </div>
-          <div className="item">
-            <img
-              src="https://pngimg.com/uploads/man/small/man_PNG6509.png"
-              alt=""
-              className="avatar"
-            />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+    )
+}
 
-export default Navbar;
+export default Navbar
 
 // prev nav:
 
