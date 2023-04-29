2fb79383df0090d755d90084135ea778d067c3a4
BadeaFreh <71319309+BadeaFreh@users.noreply.github.com>
2023-04-29 18:50:55 +0300

    make all birthdays the same format

--- src/components/ApartmentInfo/ResidentsTable/ResidentsTable.jsx
+++ src/components/ApartmentInfo/ResidentsTable/ResidentsTable.jsx
@@ -3,23 +3,25 @@ import '../../../global-styles/datagrid-table-media-queries.css'
 import { DataGrid } from '@mui/x-data-grid'
 import { Link } from 'react-router-dom'
 import { useEffect, useState } from 'react'
+import Utility from '../../../utilities/utility/util'
 
 const ResidentsTable = ({ residents }) => {
+    const utility = new Utility()
     const [rows, setRows] = useState([])
 
     useEffect(() => {
-        const filteredResidents = residents.map((r) => {
+        const filteredResidents = residents.map((resident) => {
             return {
-                _id: r._id,
-                id: r.residentId,
-                residentName: r.firstName + ' ' + r.lastName,
-                birthday: r.dateOfBirth,
+                _id: resident._id,
+                id: resident.residentId,
+                residentName: resident.firstName + ' ' + resident.lastName,
+                birthday: utility.dateFormatter(resident.dateOfBirth),
                 budget: `₪ ${
-                    r.budget > 999
-                        ? (r.budget / 1000).toFixed(1) + 'k'
-                        : r.budget
+                    resident.budget > 999
+                        ? (resident.budget / 1000).toFixed(1) + 'k'
+                        : resident.budget
                 }`,
-                img: r.image,
+                img: resident.image,
             }
         })
         setRows(filteredResidents)
