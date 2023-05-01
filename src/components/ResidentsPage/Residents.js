cd27351b0fa083898588998fa1b034d4cf0c8fb3
Braa Knaane <braa97@windowslive.com>
2023-05-01 10:25:10 +0300

    Fixed residents not showing

--- src/components/ResidentsPage/Residents.js
+++ src/components/ResidentsPage/Residents.js
@@ -1,31 +1,33 @@
 import React, { useState, useEffect } from 'react'
 import ApiManager from '../../apiManager/apiManager'
+import CoordinatorApiMan from '../../coordinatorApiManager/coordinatorApiMan'
 import LoadingWheel from '../LoadingWheel/LoadingWheel'
 import ResidentsTable from '../ApartmentInfo/ResidentsTable/ResidentsTable'
 import './Residents.css'
 
-const Residents = ({ coordinatorApartments }) => {
+const Residents = () => {
     const [residents, setResidents] = useState([])
     const [loading, setLoading] = useState(true)
-    const instructorId = JSON.parse(localStorage.getItem('user'))?.userId
+    const user = JSON.parse(localStorage.getItem('user'))
 
     useEffect(() => {
         const apiManager = new ApiManager()
+        const coordinatorApiMan = new CoordinatorApiMan()
 
         let fetchInstructorResidents = async () => {
-            let apartments = await apiManager.getResidentsByInstructorId(
-                instructorId
-            )
-            setResidents(apartments)
+            let response
+            if (user?.userType === "Instructor") {
+                response = await apiManager.getResidentsByInstructorId(user?.userId)
+            }
+            else {
+                response = await coordinatorApiMan.getResidentsByCoordinatorId(user?.userId)
+                response = response.data
+            }
+            setResidents(response)
             setLoading(false)
         }
-        if (localStorage.getItem('user')) {
-            fetchInstructorResidents()
-        } else {
-            setLoading(false)
-            setResidents(coordinatorApartments)
-        }
-    }, [coordinatorApartments, instructorId])
+        fetchInstructorResidents()
+    }, [])
     try {
         return (
             <div className='instructor-residents-component'>
